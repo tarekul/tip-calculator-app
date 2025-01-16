5a7495efbf41f05884354e19d9d2353bc50497ea
tarekul <rahin6810@gmail.com>
2025-01-16 00:21:40 -0500

    refactor code

--- main.js
+++ main.js
@@ -1,61 +1,99 @@
-document.getElementById('bill').addEventListener('input', calculate);
+document.getElementById('bill').addEventListener('input', () => handleBill());
 document.getElementById('tip-5').addEventListener('click', setTip);
 document.getElementById('tip-10').addEventListener('click', setTip);
 document.getElementById('tip-15').addEventListener('click', setTip);
 document.getElementById('tip-25').addEventListener('click', setTip);
 document.getElementById('tip-50').addEventListener('click', setTip);
 document.getElementById('custom').addEventListener('input', setTip);
-document.getElementById('people').addEventListener('input', calculate);
+document.getElementById('people').addEventListener('input', () => handleBill());
+document.getElementById('reset').addEventListener('click', reset);
 
-let tip = 0;
-function calculate() {
-    const bill = document.getElementById('bill').value;
-    const people = document.getElementById('people').value;
-    const customTip = document.getElementById('custom').value;
+function reset() {
+    document.getElementById('bill').value = '';
+    document.getElementById('people').value = '';
+    document.getElementById('custom').value = '';
+    document.getElementById('tip').innerText = '$0.00';
+    document.getElementById('total').innerText = '$0.00';
 
-    if (customTip > 0) {
-        tip = customTip;
+    const prevSelected = document.querySelector('button[name="tip"].bg-sky-700');
+    if (prevSelected) {
+        prevSelected.classList.remove('bg-sky-700');
+        prevSelected.classList.add('bg-cyan-700'); 
     }
+}
 
-    console.log(bill, tip, people);
-    if (bill > 0 && tip > 0 && people > 0) {
-        document.getElementById('tip').innerText = '$' + (bill * tip / 100 / people).toFixed(2);
-        document.getElementById('total').innerText = '$' + (bill * (tip / 100 + 1) / people).toFixed(2);
+function calculate(bill, people, tip, typeOfTip) {
+    if (bill > 0 && tip >= 0 && people > 0) {
+       tipPerPerson(bill, people, tip, typeOfTip); 
+    }
+}
+
+function handleBill() {
+    const bill = document.getElementById('bill').value;
+    const people = validatePeople();
+    const selection = document.querySelector('button[name="tip"].bg-sky-700')
+    let tip = 0;
+    let typeOfTip = '';
+    if (selection) {
+        tip = selection.id.split('-')[1];
+        typeOfTip = 'fixed';
+    } else {
+        tip = validateCustomTip();
+        typeOfTip = 'custom';
+    }
+    
+    if (bill <= 0 || people <= 0) {
+        document.getElementById('tip').innerText = '$0.00';
+        document.getElementById('total').innerText = '$0.00';
+    } else {
+        calculate(bill, people, tip, typeOfTip);
+    }
+}
+
+function tipPerPerson(bill, people, tip, typeOfTip) {
+    if (typeOfTip == 'custom') {
+        document.getElementById('tip').innerText = '$' + (Number(tip) / people).toFixed(2);
+        document.getElementById('total').innerText = '$' + ((Number(bill) + Number(tip)) / people).toFixed(2);
+    } else {
+        document.getElementById('tip').innerText = '$' + (Number(bill) * Number(tip) / 100 / people).toFixed(2);
+        document.getElementById('total').innerText = '$' + (Number(bill) * (Number(tip) / 100 + 1) / people).toFixed(2);
     }
 }
 
 function setTip() {
-    let customTip = document.getElementById('custom').value;
-    customTip = customTip.replace(/[^0-9.]/g, '');
-    customTip = validateCustomTip(customTip);
-
-    document.getElementById('custom').value = customTip;
-    const tipButtons = document.querySelectorAll('button[name="tip"]');
-    tipButtons.forEach(button => {
-        button.classList.remove('bg-sky-700');
-        button.classList.add('bg-cyan-700'); 
-
-        if(parseInt(customTip)) {
-            button.classList.remove('hover:bg-sky-700');
-        }
-    });
-
-    if (this.id !== 'custom' && !parseInt(customTip)) {
-        this.classList.add('bg-sky-700'); 
-        this.classList.remove('bg-cyan-700'); 
+    const prevSelected = document.querySelector('button[name="tip"].bg-sky-700');
+    if (prevSelected) {
+        prevSelected.classList.remove('bg-sky-700');
+        prevSelected.classList.add('bg-cyan-700'); 
     }
 
-    this.id === 'custom' ? tip = customTip : tip = parseInt(this.id.slice(4));
-    calculate();
+    if (this.id !== 'custom') {
+        document.getElementById('custom').value = '';
+        this.classList.add('bg-sky-700'); 
+    }
+
+    handleBill();
 }
 
-function validateCustomTip(customTip) {
+validatePeople = () => {
+    const people = document.getElementById('people').value;
+    document.getElementById('people').value = people.replace(/[^0-9]/g, '');
+
+    return people;
+}
+
+function validateCustomTip() {
+    let customTip = document.getElementById('custom').value;
+    customTip = customTip.replace(/[^0-9.]/g, '');
+
     if ((customTip.match(/\./g) || []).length > 1) {
         customTip = customTip.split('.').slice(0, 1).join('') + '.' + customTip.split('.').slice(1).join('');
     }
     if ((customTip.match(/\./g) || []).length == 1) {
         customTip = customTip.split('.')[0] + '.' + customTip.split('.')[1].split('').slice(0, 2).join('');
     }
+
+    document.getElementById('custom').value = customTip;
 
     return customTip;
 }
